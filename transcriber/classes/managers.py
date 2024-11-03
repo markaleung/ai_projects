2c278c286a22a810c49628d96b14e23663ca62e9
Mark Leung <markaleung@gmail.com>
2024-11-03 10:14:11 +0800

    Transcriber: use srt for movie subtitles, txt for audio

--- transcriber/classes/managers.py
+++ transcriber/classes/managers.py
@@ -8,9 +8,18 @@ class FileRunner:
     def __init__(self, filename_in: str, model: str):
         self.config = Config()
         self.config.filename_in = filename_in
-        self.config.filename_out = re.sub(r'.\w+$', '.txt', self.config.filename_in)
+        self.extension_in = filename_in.split('.')[-1]
         self.config.device = 'mps'
         self.config.model = model
+        if self.extension_in in {'mp3', 'm4a'}:
+            self.formatter_class = 'DF'
+            self.extension_out = 'txt'
+        elif self.extension_in in {'mp4'}:
+            self.formatter_class = 'SRT'
+            self.extension_out = 'srt'
+        self.config.filename_out = self.config.filename_in.replace(
+            self.extension_in, self.extension_out
+        )
     def _extract_wav(self):
         self.wav = models.Wav(config_ = self.config)
         self.wav.main()
@@ -19,7 +28,10 @@ class FileRunner:
         self.model.main()
         self.wav.delete_wav()
     def _format_srt(self):
-        self.formatter = formatters.DFFormatter(config_ = self.config)
+        self.formatter = (
+            getattr(formatters, f'{self.formatter_class}Formatter')
+            (config_ = self.config)
+        )
         self.formatter.main()
     def main(self):
         if os.path.exists(self.config.filename_out):
@@ -37,7 +49,7 @@ class FolderRunner:
         os.chdir(self.folder)
         self.paths = [
             f'{folder}/{file}' for folder, _, files in os.walk('.') 
-            for file in files if file[-3:] == 'mp3'
+            for file in files if file[-3:] in {'mp3', 'm4a', 'mp4'}
         ]
     def _run_file(self):
         self.file_runner = FileRunner(
