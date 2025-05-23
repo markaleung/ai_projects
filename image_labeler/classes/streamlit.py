d4a153d3c6734d9d4e1fab36fadc782ce350ff55
Mark Leung <markaleung@gmail.com>
2025-05-23 20:07:58 +0800

    Image: Rearrange make widgets function

--- image_labeler/classes/streamlit.py
+++ image_labeler/classes/streamlit.py
@@ -7,8 +7,17 @@ class ImageNet:
     def _read_df(self):
         os.chdir(os.path.dirname(__file__))
         self.df = pd.read_csv('../files_output/labels.csv')
-    # Make Widgets and Helpers
-    def _get_groups(self):
+    def _set_variables_ui(self):
+        if st.button('Refresh'):
+            st.rerun()
+        self.threshold = st.number_input(
+            'Threshold', min_value = 0.0, max_value = 1.0, value = 0.1
+        )
+        self.mode = st.radio(
+            'Choose Label', ['Random', 'Dropdown'], horizontal = True
+        )
+    def _filter_and_group_df(self):
+        self.df = self.df.query('score > @self.threshold')
         self.grouped = (
             self.df.groupby('label').size()
             .to_frame('size_').reset_index()
@@ -22,22 +31,12 @@ class ImageNet:
     def _get_label_dropdown(self):   
         self.label = st.selectbox('Label', self.grouped.label)
         self.grouped2 = self.grouped.query('label == @self.label')
-    def _make_widgets(self):
-        if st.button('Refresh'):
-            st.rerun()
-        self.threshold = st.number_input(
-            'Threshold', min_value = 0.0, max_value = 1.0, value = 0.1
-        )
-        self.df = self.df.query('score > @self.threshold')
-        self._get_groups()
-        self.mode = st.radio(
-            'Choose Label', ['Random', 'Dropdown'], horizontal = True
-        )
+    def _get_label(self):
         if self.mode == 'Random':
             self._get_label_random()
         else:
             self._get_label_dropdown()
-    # After Make Widgets
+        st.write(self.grouped2.combined.values[0])
     def _get_paths(self):
         self.df_filtered = self.df.query('label == @self.label')
         self.filenames = (
@@ -50,7 +49,6 @@ class ImageNet:
             for filename in self.filenames
         ]
     def _write(self):
-        st.write(self.grouped2.combined.values[0])
         self.col1, self.col2, self.col3 = st.columns(3)
         with self.col1:
             st.image(self.paths[0:5])
@@ -61,7 +59,9 @@ class ImageNet:
     def main(self):
         self._read_df()
         with st.sidebar:
-            self._make_widgets()
+            self._set_variables_ui()
+            self._filter_and_group_df()
+            self._get_label()
         self._get_paths()
         self._write()
 
